2cb23e375446f9be94a3184db63356f9a9b9ba7f
nirmanirajakaruna <nirmanirajakaruna217@gmail.com>
2025-12-10 00:52:51 +0530

    task 2(2p) task 3(3p) task 4(3p) task 5(3p) completed

--- Module 1/t2.js
+++ Module 1/t2.js
@@ -1,13 +1,9 @@
 function greetUser() {
-            const name = prompt("Please enter your name:");
+    const name = prompt("Please enter your name:");
 
-            if (name) {
-
-                document.getElementById("output1").innerHTML = `Hello, ${name}!`;
-            } else {
-                document.getElementById("output1").innerHTML = "No name entered.";
-            }
-        }
-    </script>
-</body>
-</html>
+    if (name) {
+        document.getElementById("output1").innerHTML = `Hello, ${name}!`;
+    } else {
+        document.getElementById("output1").innerHTML = "No name entered.";
+    }
+}
