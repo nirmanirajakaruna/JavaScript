6b471d79c5f0160e878d320dc125205e9eda3dc0
nirmanirajakaruna <nirmanirajakaruna217@gmail.com>
2025-12-09 23:24:50 +0530

    first commit

---
+++ Module 1/t2.js
@@ -0,0 +1,13 @@
+function greetUser() {
+            const name = prompt("Please enter your name:");
+
+            if (name) {
+
+                document.getElementById("output1").innerHTML = `Hello, ${name}!`;
+            } else {
+                document.getElementById("output1").innerHTML = "No name entered.";
+            }
+        }
+    </script>
+</body>
+</html>
